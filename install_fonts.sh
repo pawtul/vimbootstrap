a29eef042f6ae29edcbfda8e2bf1e01ca4550c10
pawtul <pawtul@gmail.com>
2016-10-27 06:38:18 -0700

    vimbootstrap

---
+++ install_fonts.sh
@@ -0,0 +1,6 @@
+#copy to /usr/local/share/fonts/ for system-wide installation
+mkdir ~/.fonts
+
+cp ./fonts/* ~/.fonts/
+
+fc-cache -f -v
---
+++ main.sh
@@ -0,0 +1,18 @@
+bash install_fonts.sh
+
+mkdir -p ~/.vim/bundle
+
+git clone https://pawtul@bitbucket.org/pawtul/dotfiles.git
+
+if [ -f ~/.vimrc ]; then
+    mv .vimrc .vimrc-old
+fi
+mv dotfiles/.vimrc ~
+
+cd ~/.vim/bundle
+
+git clone https://github.com/VundleVim/Vundle.vim.git
+
+echo "Your setup is ready."
+echo "enter your vim and type :PluginInstall"
+
