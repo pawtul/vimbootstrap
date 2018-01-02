539286435654b89061edac66a9997dd312640d97
pawel <pawel@localhost.localdomain>
2018-01-02 13:02:07 +0000

    Added downloading of plugins

--- install_plugins.sh
+++ install_plugins.sh
@@ -3,6 +3,12 @@ cd ~/.vim/bundle
 
 git clone https://github.com/VundleVim/Vundle.vim.git
 
-echo "Your setup is ready."
-echo "enter your vim and type :PluginInstall"
+github_base_url=https://github.com/
 
+for plugin in `cat ~/.vimrc | sed -n "s/^Plugin '\(\S*\)' .*$/\1/p"`;
+do
+    git clone $github_base_url$plugin
+done;
+
+#echo "Your setup is ready."
+#echo "enter your vim and type :PluginInstall"
