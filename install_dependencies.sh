10a513f4563254fd964777103849f6238d1ea9cc
Pawel Tulowiecki <pawtul@gmail.com>
2018-05-07 14:17:47 +0000

    added installation of pip

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,13 @@
 sudo apt-get update
-sudo apt-get -y install libncurses5-dev libncursesw5-dev ack-grep gcc python3-dev python-dev
+sudo apt-get -y install libncurses5-dev libncursesw5-dev ack-grep gcc python3-dev python-dev exuberant-ctags
 
-python3 -m pip install mypy flake8 pylint isort typing
-python -m pip install mypy flake8 pylint isort typing
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+sudo python3 get-pip.py
+sudo python2 get-pip.py
+
+rm get-pip.py
+
+sudo python3 -m pip install mypy flake8 pylint isort typing
+sudo python2 -m pip install flake8 pylint isort typing
 
