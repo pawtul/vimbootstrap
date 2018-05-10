cfe7654fb3824178298c659cd916bef273173979
Paweł Tułowiecki <pawtul@gmail.com>
2018-05-10 20:38:43 +0200

    added backup for old dotfiles

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,16 @@
 git clone https://github.com/pawtul/dotfiles.git
+
+# backup old files
+if [[ -e ~/.vimrc ]]; then
+    mv ~/.vimrc ~/.vimrc-old
+fi
+if [[ -e ~/.config/flake8 ]]; then
+    mv ~/.config/flake8 ~/.config/flake8-old
+fi
+if [[ -e ~/.config/pylintrc ]]; then
+    mv ~/.config/pylintrc ~/.config/pylintrc-old
+fi
+
 cp dotfiles/.vimrc ~
 cp dotfiles/flake8 ~/.config/
 cp dotfiles/pylintrc ~/.config/
